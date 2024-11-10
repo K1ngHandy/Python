347e676775ea2937ed9503e1367c7e2eb84b09d7
K1ngHandy <bolds-slicks0g@icloud.com>
2024-11-10 18:22:58 -0500

    Initial commit

---
+++ Variables/Arithmetic operators/arithmetic_operators.py
@@ -0,0 +1,17 @@
+init_number = 9.0        # Float number
+
+division_result = init_number / 2.0
+
+division_remainder = init_number % 2.0
+
+multiplication_result = division_result * 2.0
+
+addition_result = multiplication_result + division_remainder
+
+subtraction_result = init_number - multiplication_result
+
+floor_result = init_number // 2
+
+power_result = multiplication_result ** 3
+
+print("result = " + str(subtraction_result))
